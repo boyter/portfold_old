3d0c9e2d0bb9d5c84449edb6cdb84243980705e6
Ben Boyter <ben@boyter.org>
2018-12-05 18:47:41 +1100

    Start to build out basic db connection

--- data/mysql/project.go
+++ data/mysql/project.go
@@ -14,5 +14,23 @@ func (m *ProjectModel) Insert(project data.Project) (*data.Project, error) {
 }
 
 func (m *ProjectModel) Get(id int) (*data.Project, error) {
-	return nil, nil
+	stmt := `
+		SELECT	id,
+             	name,
+				created,
+				updated
+		FROM	project
+		WHERE	id = ?
+`
+	row := m.DB.QueryRow(stmt, id)
+	project := &data.Project{}
+
+	err := row.Scan(&project.Id, &project.Name, &project.Created, &project.Updated)
+	if err == sql.ErrNoRows {
+		return nil, data.ErrNoRecord
+	} else if err != nil {
+		return nil, err
+	}
+
+	return project, nil
 }
