c97a45bb9ca29a73fd26b8fb2ade99a3640e7de3
Ben Boyter <ben@boyter.org>
2018-12-07 08:05:13 +1100

    Add in middlware chainer for cookies

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ require (
 	github.com/golangcollege/sessions v1.0.1
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
+	github.com/justinas/alice v0.0.0-20171023064455-03f45bd4b7da
 	golang.org/x/crypto v0.0.0-20181203042331-505ab145d0a9 // indirect
 	google.golang.org/appengine v1.3.0 // indirect
 )
--- handlers/handler.go
+++ handlers/handler.go
@@ -1,45 +1,11 @@
 package handlers
 
 import (
-	"boyter/portfold/data/mysql"
 	"boyter/portfold/forms"
-	"github.com/golangcollege/sessions"
-	"github.com/gorilla/mux"
 	"html/template"
-	"log"
 	"net/http"
 )
 
-// Define an application struct to hold the application-wide dependencies for the
-// web application. For now we'll only include fields for the two custom loggers, but
-// we'll add more to it as the build progresses.
-type Application struct {
-	ErrorLog     *log.Logger
-	InfoLog      *log.Logger
-	ProjectModel *mysql.ProjectModel
-	Session      *sessions.Session
-}
-
-func (app *Application) Routes() *mux.Router {
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
-	// It is good practice to create a new one to avoid the default global one
-	// being polluted by imports
-	router := mux.NewRouter()
-	router.Handle("/", http.HandlerFunc(app.Home)).Methods("GET")
-	router.Handle("/help/", http.HandlerFunc(app.Help)).Methods("GET")
-	router.Handle("/health-check/", http.HandlerFunc(app.HealthCheck)).Methods("GET")
-
-	// User login
-	router.Handle("/user/login/", http.HandlerFunc(app.LoginForm)).Methods("GET")
-	router.Handle("/user/login/", http.HandlerFunc(app.LoginUser)).Methods("POST")
-
-	// Setup to serve files from the supplied directory
-	fileServer := http.FileServer(http.Dir("./assets/ui/static/"))
-	router.PathPrefix("/static/").Handler(http.StripPrefix("/static", fileServer))
-
-	return router
-}
 
 func (app *Application) LoginForm(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Show the login form here"))
