3142d296e5eca0efe8480246e8f63366e9b91c80
Ben Boyter <ben@boyter.org>
2018-12-07 17:42:13 +1100

    Comment the shared test file

--- data/mysql/shared_test.go
+++ data/mysql/shared_test.go
@@ -8,8 +8,13 @@ import (
 	"testing"
 )
 
+// For Model integration tests we need to be able to connect to the
+// database but also want to avoid using all the connections
+// so we have this to share it and ensure that we only connect once
+
 var db *sql.DB = nil
 
+// NB this method is not thread safe
 func connect(t *testing.T) *sql.DB {
 	if db != nil {
 		return db
